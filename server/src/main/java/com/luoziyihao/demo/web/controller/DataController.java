9a648b1205501af7b69c741ed1524f60d651ccce
luoziyihao <wangyiraoxiang@163.com>
2019-06-23 00:38:33 +0800

    tmp push

--- server/src/main/java/com/luoziyihao/demo/web/controller/DataController.java
+++ server/src/main/java/com/luoziyihao/demo/web/controller/DataController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RequestMapping("/api/data")
 @RestController
 public class DataController {
+
     private List<String> dataLists = new ArrayList<>();
 
     @RequestMapping("/add")
