30bcddfa018b7bf08cd7d660d93f8f7c545192d3
luoziyihao <wangyiraoxiang@163.com>
2019-06-23 00:30:59 +0800

    tmp push

--- server/src/main/java/com/luoziyihao/demo/web/controller/DataController.java
+++ server/src/main/java/com/luoziyihao/demo/web/controller/DataController.java
@@ -13,22 +13,20 @@ import java.util.List;
  * @author xiang.rao created on 3/4/18 11:12 PM
  * @version $Id$
  */
-@RequestMapping("/data")
+@RequestMapping("/api/data")
 @RestController
 public class DataController {
-
-
     private List<String> dataLists = new ArrayList<>();
 
     @RequestMapping("/add")
-    public Object add(String text) {
+    public ApiResult add(String text) {
         dataLists.add(text);
         return ApiResult.success();
     }
 
 
     @RequestMapping("/query")
-    public Object query() {
+    public ApiResult query() {
         HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
         objectObjectHashMap.put("text", Arrays.toString(dataLists.toArray()));
         return ApiResult.success(objectObjectHashMap);
